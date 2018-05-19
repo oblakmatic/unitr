f9f9c7f9406ef230e0376bdc64ecdc7d1df67f27
aljazv <aljaz.verlic@gmail.com>
2018-05-19 18:37:53 +0200

    first

---
+++ script.sh
@@ -0,0 +1,8 @@
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+
+rm db.sqlite3
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminadmin', first_name='Skrbnik')" | python3 manage.py shell
---
+++ spletna/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SpletnaConfig(AppConfig):
+    name = 'spletna'
