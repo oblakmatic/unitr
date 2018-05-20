b4e47567b936d5ff5f7773f50bf47b6df3204a4a
Unknown <aljaz.verlic@gmail.com>
2018-05-20 06:01:34 +0200

    ads

--- spletna/socket.py
+++ spletna/socket.py
@@ -9,4 +9,9 @@ def naredi_streznik(request):
     print("enov")
     server = WebsocketServer(13254, host='127.0.0.1', loglevel=logging.INFO)
     server.set_fn_new_client(new_client)
+    server.set_fn_message_received(prejetoSporocilo)
     server.run_forever()
+
+
+def prejetoSporocilo(client, server):
+    server.send_message_to_all("jo jo jo kje ste ")
